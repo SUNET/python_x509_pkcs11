dcdf04f5d570b92deeecc109aaef1faa2e321233
Kushal Das <mail@kushaldas.in>
2024-02-13 20:56:37 +0100

    Updates exception docstrings

--- src/python_x509_pkcs11/error.py
+++ src/python_x509_pkcs11/error.py
@@ -4,24 +4,24 @@ Module which have all our exceptions
 
 
 class PKCS11UnknownErrorException(Exception):
-    """Class to handle PKCS11 exceptions"""
+    """Exception related to PKCS11 operations."""
 
-    def __init__(self, message: str = "PKCS11 Exception happened") -> None:
+    def __init__(self, message: str = "PKCS11 Exception happened.") -> None:
         self.message = message
         super().__init__(self.message)
 
 
 class DuplicateExtensionException(Exception):
-    """Class to handle PKCS11 timeout exceptions"""
+    """Exception due to duplicate extensions."""
 
-    def __init__(self, message: str = "Duplicate extension not allowed") -> None:
+    def __init__(self, message: str = "Duplicate extension not allowed.") -> None:
         self.message = message
         super().__init__(self.message)
 
 
 class OCSPMissingExtensionException(Exception):
-    """Class to handle OCSP missing extension exceptions"""
+    """Exception for missing OCSP extension."""
 
-    def __init__(self, message: str = "Required OCSP extension not found") -> None:
+    def __init__(self, message: str = "Required OCSP extension not found.") -> None:
         self.message = message
         super().__init__(self.message)
